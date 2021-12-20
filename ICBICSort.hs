dddf0e36155150024f74690e2d53ebb46ca51a10
cutsea110 <cutsea110@gmail.com>
2021-12-21 07:43:21 +0900

    add type signature and sample test

--- ICBICSort.hs
+++ ICBICSort.hs
@@ -10,7 +10,20 @@ debug = True
 ($?) :: Show a => (a -> b) -> a -> b
 f $? x = if debug then trace (show x) (f x) else f x
 
--- I Can't Believe It Can Sort Algorithm.
+
+{- | I Can't Believe It Can Sort Algorithm.
+>>> sort [1,3,2,5,4,7,6,9]
+([],[1,3,2,5,4,7,6,0])
+([7],[1,2,3,4,5,6,0])
+([1,7],[2,3,4,5,6,0])
+([1,2,7],[3,4,5,6,0])
+([1,2,3,7],[4,5,6,0])
+([1,2,3,4,7],[5,6,0])
+([1,2,3,4,5,7],[6,0])
+([1,2,3,4,5,6,7],[0])
+([0,1,2,3,4,5,6,7],[])
+[0,1,2,3,4,5,6,7]
+-}
 sort :: (Show a, Ord a) => [a] -> [a]
 sort xs = swapper $? ([], xs)
 
@@ -26,4 +39,5 @@ swap (x, xs) = case break (x<) xs of
   (xs', y:ys) -> f (x, xs') $ swap (y, ys)
     where f (x, xs) (y, ys) = (y, xs++[x]++ys)
 
+sample :: [Int]
 sample = [1,3,2,5,4,7,6,0]
